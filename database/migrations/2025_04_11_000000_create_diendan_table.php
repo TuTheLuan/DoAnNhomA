a500332caaf917e798d44c342d5174bd2b442239
Keys-ping <huynhthaiquoc.tdc.2223@gmail.com>
2025-04-18 08:43:05 +0700

    update

---
+++ database/migrations/2025_04_11_000000_create_diendan_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('diendan', function (Blueprint $table) {
+            $table->id();
+            $table->string('ma_dien_dan', 20)->unique();
+            $table->string('ten_dien_dan');
+            $table->enum('loai_thao_luan', ['public', 'anonymous'])->default('public');
+            $table->date('ngay_tao');
+            $table->string('background_image')->nullable();
+            $table->json('images')->nullable(); // Thêm trường lưu nhiều ảnh dạng JSON
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('diendan');
+    }
+};
